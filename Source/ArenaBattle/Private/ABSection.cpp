1abce6174f7dfaefc7f0bdabc4eb10af4a058cb8
svvj <wjdtmd0105@gmail.com>
2022-01-17 22:01:02 +0900

    Infinity level 1

---
+++ Source/ArenaBattle/Private/ABSection.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABSection.h"
+
+// Sets default values
+AABSection::AABSection()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
+	RootComponent = Mesh;
+
+	FString AssetPath = TEXT("/Game/Book/StaticMesh/SM_SQUARE.SM_SQUARE");
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_SQUARE(*AssetPath);
+	if (SM_SQUARE.Succeeded())
+	{
+		Mesh->SetStaticMesh(SM_SQUARE.Object);
+	}
+	else
+	{
+		ABLOG(Error, TEXT("Failed to load staticmesh asset. : %s"), *AssetPath);
+	}
+}
+
+// Called when the game starts or when spawned
+void AABSection::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AABSection::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
